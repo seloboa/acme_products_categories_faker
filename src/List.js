40303b7d4156a42918acba80bab5b94100eff641
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-03-03 01:32:47 -0500

    added delete onclick handler forr categories

--- src/List.js
+++ src/List.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 
 class List extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      products: [],
+    };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
   }
   render() {
-    return this.props.categories.map(category => <li>{category.name}</li>);
+    return this.props.categories.map(category => (
+      <li key={category.id}>
+        {category.name}
+        <button id={category.id} onClick={this.handleCreate}>
+          +
+        </button>
+        <button id={category.id} onClick={this.handleDelete}>
+          -
+        </button>
+      </li>
+    ));
+  }
+  handleDelete(e) {
+    axios.delete('/api/categories', {
+      id: e.target.id,
+    });
+  }
+  handleCreate() {
+    // const cat = faker.commerce.productName();
+    // axios.post('/api/product', {name: cat});
+    // axios.get('/api/product').then(newData => {
+    //   this.setState({
+    //     categories: newData.data,
+    //   });
+    // });
   }
 }
 
