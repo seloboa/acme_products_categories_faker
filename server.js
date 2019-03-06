b61eabb82ad0759f2b86344f8831ddc14db55779
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-03-05 19:26:23 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
 
 //db
