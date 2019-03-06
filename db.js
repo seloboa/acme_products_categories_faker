679cfc53ddf1d3077522703046548cc232e8f704
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-03-05 19:02:52 -0500

    changed db to heroku db

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost/acmecatfaker', {
+const db = new Sequelize('postgres://bfopgberpbwdcx:7c7c8a847fe2f580d663355e98e147050aac8becf099d419e0a1c7dd116f8c01@ec2-54-83-196-179.compute-1.amazonaws.com:5432/deit7fk5375fkg', {
   logging: false,
 });
 
